a43e68bb6154d8ceffadb6dacef74324db5b6d36
HardcoreMagazine <HardcoreMagazine@users.noreply.github.com>
2022-05-29 00:22:26 +0600

    Add project files.

---
+++ LaunchPoint.cs
@@ -0,0 +1,10 @@
+﻿namespace Enlashceoc
+{
+    internal class LaunchPoint
+    {
+        static void Main(string[] args)
+        {
+            Menu menu = new Menu();
+        }
+    }
+}
---
+++ Menu.cs
@@ -0,0 +1,31 @@
+﻿namespace Enlashceoc
+{
+    // Main game menu
+    internal class Menu
+    {
+        static void GenerateField()
+        {
+            Console.Title = "Enlashceoc";
+            ConsoleKey pressedKey = Console.ReadKey().Key;
+        }
+        static void ActionNewGame()
+        {
+            //
+        }
+        static void ActionScoreboard()
+        {
+            //
+        }
+        static void ActionExit()
+        {
+            //
+        }
+
+
+        // Self-initialization
+        public Menu()
+        {
+            GenerateField();
+        }
+    }
+}

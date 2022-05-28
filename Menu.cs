c6c4175a72b2d86d754a374b79c38a49c7b5b902
HardcoreMagazine <HardcoreMagazine@users.noreply.github.com>
2022-05-29 00:42:08 +0600

    added game title to menu (ASCII art)

--- Menu.cs
+++ Menu.cs
@@ -6,6 +6,17 @@
         static void GenerateField()
         {
             Console.Title = "Enlashceoc";
+            Console.WriteLine(
+            "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n" +
+            "   ______       _           _\n" +
+            "  |  ____|     | |         | |\n" +
+            "  | |__   _ __ | | __ _ ___| |__   ___ ___  ___   ___\n" + 
+            "  |  __| | '_ \\| |/ _` / __| '_ \\ / __/ _ \\/ _ \\ / __|\n" + 
+            "  | |____| | | | | (_| \\__ \\ | | | (_|  __/ (_) | (__\n" + 
+            "  |______|_| |_|_|\\__,_|___/_| |_|\\___\\___|\\___/ \\___|\n" +
+            "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n\n\n");
+            Console.WriteLine("ascii-art-menu-actions");
+            //arrow key handler
             ConsoleKey pressedKey = Console.ReadKey().Key;
         }
         static void ActionNewGame()

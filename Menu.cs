60db1d28e0fce6b5225b39f5dbde9e3072242dd5
HardcoreMagazine <HardcoreMagazine@users.noreply.github.com>
2022-05-30 00:43:23 +0600

    menu progress

--- Menu.cs
+++ Menu.cs
@@ -3,20 +3,32 @@
     // Main game menu
     internal class Menu
     {
-        static void GenerateField()
+        static void GenerateField(string menuActions)
         {
+            string borderLine = 
+                "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" +
+                "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" +
+                "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@";
+            string gameTitle =
+                borderLine +
+                "\n\n" +
+                "\t\t\t\t   ______       _           _\n" +
+                "\t\t\t\t  |  ____|     | |         | |\n" +
+                "\t\t\t\t  | |__   _ __ | | __ _ ___| |__   ___ ___  ___   ___\n" +
+                "\t\t\t\t  |  __| | '_ \\| |/ _` / __| '_ \\ / __/ _ \\/ _ \\ / __|\n" +
+                "\t\t\t\t  | |____| | | | | (_| \\__ \\ | | | (_|  __/ (_) | (__\n" +
+                "\t\t\t\t  |______|_| |_|_|\\__,_|___/_| |_|\\___\\___|\\___/ \\___|\n\n" +
+                borderLine +
+                "\n\n\n";
+
+            //-----------------------------------------------//
             Console.Title = "Enlashceoc";
-            Console.WriteLine(
-            "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n" +
-            "   ______       _           _\n" +
-            "  |  ____|     | |         | |\n" +
-            "  | |__   _ __ | | __ _ ___| |__   ___ ___  ___   ___\n" + 
-            "  |  __| | '_ \\| |/ _` / __| '_ \\ / __/ _ \\/ _ \\ / __|\n" + 
-            "  | |____| | | | | (_| \\__ \\ | | | (_|  __/ (_) | (__\n" + 
-            "  |______|_| |_|_|\\__,_|___/_| |_|\\___\\___|\\___/ \\___|\n" +
-            "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n\n\n");
-            Console.WriteLine("ascii-art-menu-actions");
-            //arrow key handler
+            Console.WriteLine(gameTitle);
+            Console.WriteLine(menuActions);
+            Console.Write("\n\n\n\n\n\n\n\n" +
+                          "version: 0.1\n" +
+                          borderLine);
+            //~arrow key handler
             ConsoleKey pressedKey = Console.ReadKey().Key;
         }
         static void ActionNewGame()
@@ -36,7 +48,52 @@
         // Self-initialization
         public Menu()
         {
-            GenerateField();
+            List<string> menuActions = new List<string>
+            {
+                //ng
+                "\t\t\t\t\t\t" +
+                "> NEW GAME\n\n" +
+                "\t\t\t\t\t\t" +
+                "SCOREBOARD\n\n" +
+                "\t\t\t\t\t\t" +
+                "EXIT\n\n",
+                //sb
+                "\t\t\t\t\t\t" +
+                "NEW GAME\n\n" +
+                "\t\t\t\t\t\t" +
+                "> SCOREBOARD\n\n" +
+                "\t\t\t\t\t\t" +
+                "EXIT\n\n",
+                //exit
+                "\t\t\t\t\t\t" +
+               "NEW GAME\n\n" +
+               "\t\t\t\t\t\t" +
+               "SCOREBOARD\n\n" +
+               "\t\t\t\t\t\t" +
+               "> EXIT\n\n"
+            };
+            while (true)
+            {
+                GenerateField(menuActions[0]);
+                ConsoleKey consoleKey = Console.ReadKey().Key;
+                switch (consoleKey)
+                {
+                    case ConsoleKey.Enter:
+                        //
+                        Console.Clear();
+                        break;
+                    case ConsoleKey.UpArrow:
+                        //
+                        Console.Clear();
+                        break;
+                    case ConsoleKey.DownArrow:
+                        //
+                        Console.Clear();
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
     }
 }
---
+++ Scoreboard.cs
@@ -0,0 +1,7 @@
+﻿namespace Enlashceoc
+{
+    internal class Scoreboard
+    {
+        //
+    }
+}
